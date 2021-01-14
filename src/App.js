b4079b222826dfd78dd5fddfacf5e968e8992d20
Branyer <branyervergarac@gmail.com>
2021-01-14 14:43:02 -0400

    adding styles to the sign in page and configuring firebase sdk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="App"></div>;
+import React from "react";
+import app from "./firebase-config";
+import "firebase/auth";
+import "./styles.css";
+import googleSVG from "./buscar.svg";
+
+const provider = new app.auth.GoogleAuthProvider();
+
+const signInWithGoogleAccount = (app, provider) => 
+
+{
+
+
+
 }
 
+
+const App = () => {
+  return (
+    <div className="container">
+      <button className="sign-in--button" onClick={()=> signInWithGoogleAccount(app, provider)}>
+        <img src={googleSVG} alt="google-icon"></img>Sign In With Google
+        Account
+      </button>
+    </div>
+  );
+};
 export default App;
