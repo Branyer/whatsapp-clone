bf208533b6dd9e6609edf1ad319d4df2f7673d1b
Branyer <branyervergarac@gmail.com>
2021-01-14 15:12:32 -0400

    adding firebase authentication with google

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from "react";
-import app from "./firebase-config";
+import firebase from "./firebase-config";
 import "firebase/auth";
 import "./styles.css";
 import googleSVG from "./buscar.svg";
+import {useAuthState} from 'react-firebase-hooks/auth'
 
-const provider = new app.auth.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-const signInWithGoogleAccount = (app, provider) => 
-
-{
-
-
-
+const SignIn = (firebase, provider) => {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    console.log(error);
+    // ...
+  });
 }
-
-
 const App = () => {
+
+  const [user, loading, error] = useAuthState(firebase.auth());
+  console.log(user);
+  console.log(error);
+
+
   return (
     <div className="container">
-      <button className="sign-in--button" onClick={()=> signInWithGoogleAccount(app, provider)}>
-        <img src={googleSVG} alt="google-icon"></img>Sign In With Google
-        Account
+      <button className="sign-in--button" onClick={() => SignIn(firebase, provider)}>
+        <img src={googleSVG} alt="google-icon"></img>Sign In With Google Account
       </button>
     </div>
   );
