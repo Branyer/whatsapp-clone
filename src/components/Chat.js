68ca2880a9ce4713eda6e69eb845c81b9144eda2
Branyer <branyervergarac@gmail.com>
2021-01-16 18:46:08 -0400

    adding chat-no-selected view to the Chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import whatsappSVG from "../images/whatsapp-icon.svg";
+import "../styles/chat.css";
 
 const Chat = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  //TODO manejar el cambio de chat
 
-export default Chat
+  return (
+    <div className="no-chat-selected">
+      <div>
+        <img alt="whatsapp" src={whatsappSVG}></img>
+        <span>Select a chat to start messaging</span>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
